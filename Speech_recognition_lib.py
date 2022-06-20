c0c48136505c5b82fab3997e1677457a7b0c4d28
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-06-20 22:33:25 +0700

    Add files via upload

--- Speech_recognition_lib.py
+++ Speech_recognition_lib.py
@@ -1,11 +1,11 @@
 import threading
-from roboreactmaster import Speech_recognition,Create_node_sub
+from roboreactmaster import Speech_recognition,Create_node_sub,Speaking_languages
 from itertools import count
 
 def speech_recog():
      for rt in count(0):
         try:
-          Speech_recognition('th','en','127.0.0.1',5630)
+          Speech_recognition('th','127.0.0.1',5630)
         except:
             pass 
 def Speech_node():
@@ -13,6 +13,10 @@ def Speech_node():
         for rt in count(0):
             global data_out;data_out = Create_node_sub(1,"127.0.0.1",4096,5630)
             print("speech_to_text ",data_out)
+            speech_key = list(data_out)[0]
+            if speech_key != " ":
+                 Speaking_languages(speech_key,'th','1.04',100) 
+            
 t1 = threading.Thread(target=speech_recog)
 t2 = threading.Thread(target=Speech_node)
 t1.start() 
